ec8c319cab612a80c46f0c3f7c7decbc93f1215f
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-03-23 18:40:31 +0000

    tailwind props

--- 03-tailwind_props/src/App.jsx
+++ 03-tailwind_props/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/Card.jsx'
+//using devUI library for cards
 function App() {
   const [count, setCount] = useState(0)
-
+  let myobj={name:"om",age:21} 
+  let newarr=[1,2,3,4]
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card username="chai aur code" someOb={newarr} />
+      <Card  username="OM" btntext="visitme"/>
     </>
   )
 }
