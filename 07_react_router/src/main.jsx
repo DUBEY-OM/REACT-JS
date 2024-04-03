265e708d434c43e734bce9e6a06ccfe5b09a063a
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-04-03 17:59:48 +0000

    ok

--- 07_react_router/src/main.jsx
+++ 07_react_router/src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Layout from './components/Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-
+import Layout from './Components/Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/ContactUs/Contactme.jsx'
+import User from './Components/User/User.jsx'
+import Github from './Components/Github/Github.jsx'
 import './index.css'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
-const router=createBrowserRouter([{path:'/',element:<Layout/>,childern:[{path:"",element:<Home/>},{path:"",element:<About/>}]}])
+import { Route, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+// const router=createBrowserRouter([{path:'/',element:<Layout/>,childern:[{path:"",element:<Home/>},{path:"/About",element:<About/>},{path:"/contact",element:<Contact/>}]}])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />} >
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github/>} />
+   </Route>))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>,)
