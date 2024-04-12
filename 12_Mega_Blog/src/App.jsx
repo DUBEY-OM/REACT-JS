e8dec481d662ab5f599385c28cde7960edc63a8c
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-04-12 19:13:22 +0000

    OK DONE FOR TODAY

---
+++ 12_Mega_Blog/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+function App() {
+console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+
+  return (
+    <>
+      
+      <div className="App">LETS GO</div>
+    </>
+  )
+}
+
+export default App
